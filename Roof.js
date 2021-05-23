41f69b29b16f6fd9b9698871eb0166ae3dc2eb0e
yuvraj931 <76772160+yuvraj931@users.noreply.github.com>
2021-05-23 18:05:53 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,21 @@
+class Roof{
+  constructor(x,y,width,height){
+    var options = {
+      isStatic:true
+    }
+
+     this.body = Bodies.rectangle(x,y,width,height,options)
+     this.width = width;
+     this.height = height;
+     World.add(world,this.body);
+     
+ }
+  display(){
+     push();
+     rectMode(CENTER);
+     fill(128,128,128);
+     rect(this.body.position.x,this.body.position.y,this.width,this.height);
+     pop()
+ }
+
+ }
